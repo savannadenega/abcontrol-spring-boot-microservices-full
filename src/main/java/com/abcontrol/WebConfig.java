b38f93000b38e606e92edac4e03c5cb63580b5e5
Varisco <fabio.varisco@sap.com>
2018-11-18 19:29:12 -0200

    Add CSR config

---
+++ src/main/java/com/abcontrol/WebConfig.java
@@ -0,0 +1,18 @@
+package com.abcontrol;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:4000", "https://abcontrol-spring-boot-web-full.herokuapp.com/")
+			.allowedMethods("GET", "POST", "PUT", "DELETE");
+	}
+}
